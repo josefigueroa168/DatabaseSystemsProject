4ed03489c949205830fc7bfa553b6e397d32b2c6
Matthew Garber <matthewgarber@heffner-wl-30.dynamic.rpi.edu>
2019-03-18 15:43:12 -0400

    Creates database and user

--- load_data.py
+++ load_data.py
@@ -2,26 +2,29 @@ import csv
 import psycopg2
 import psycopg2.extras
 
-'''
-DROP DATABASE IF EXISTS health;
-CREATE DATABASE restaurant;
 
-DROP USER IF EXISTS health;
-CREATE USER health WITH PASSWORD 'health';
-
-GRANT ALL PRIVILEGES ON DATABASE health TO health;
-'''
+conn_string = "host='localhost' dbname='baseball' user='postgres' password='postgres'"
 
 
-connection_string = "host='localhost' dbname='restaurant' user='health' password='health'"
-conn = psycopg2.connect(connection_string, cursor_factory=psycopg2.extras.DictCursor)
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+conn.autocommit = True
+
+cursor.execute("DROP DATABASE IF EXISTS health;")
+cursor.execute("CREATE DATABASE health;")
+
+cursor.execute("DROP USER IF EXISTS health;")
+cursor.execute("CREATE USER health WITH PASSWORD 'health';")
+
+cursor.execute("GRANT ALL PRIVILEGES ON DATABASE health TO health;")
+
+conn.autocommit = False
 
 with open('Healthy_Aging_Data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
             year = row[0]
@@ -30,8 +33,6 @@ with open('Healthy_Aging_Data.csv') as csv_file:
             topic = row[6]
             question = row[7]
             answer = row[12]
-            print(year, location, classType, topic, question, answer)
-            
-            #print(f'\t{row[0]}, {row[1]}, {row[2]}.')
             line_count += 1
-    print(f'Processed {line_count} lines.')
+
+cursor.close()
