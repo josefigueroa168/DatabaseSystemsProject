e3ac9a4f9f1a7b1bf1291aaa142aadf610d74a13
Jose Figueroa <josefigueroa168@gmail.com>
2019-03-22 16:22:09 -0400

    comment removal

--- load_data.py
+++ load_data.py
@@ -49,18 +49,6 @@ with open('Healthy_Aging_Data.csv') as csv_file:
             answer = row[12]
             line_count += 1
 
-'''
-with open('500_Cities__Local_Data_for_Better_Health__2018_release.csv') as csv_file:
-    cities_df = pd.read_csv(csv_file, usecols=['Year','StateDesc', 'StateAbbr','CityName', 'CityFIPS','PopulationCount','Category','CategoryID', 'MeasureId', 'Measure','Short_Question_Text'  ])
-    #Insert to state column
-    state = cities_df.loc[:,['StateAbbr','StateDesc']].drop_duplicates('StateAbbr')
-    #Insert to cities.. so on. Should clean US dates.
-    city_state = cities_df.loc[:, ['CityFIPS', 'CityName', 'StateAbbr']].drop_duplicates('CityFIPS')
-
-for index, row in state.itertuples():
-    cursor.execute("INSERT INTO TABLE state (stateAbbr, stateName)
-    VALUES ('{0}', '{1}')".format(row['StateAbbr'], row['StateDesc']))
-'''
 insertStates(cursor, conn)
 
 
