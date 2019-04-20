7c927a622879183ac76dc64813cee68a8674fd4a
Jose Figueroa <josefigueroa168@gmail.com>
2019-04-20 11:18:34 -0400

    Added simple connection close

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ class database(object):
         conn_string = "host='localhost' dbname='health' user='health' password='health'"
         self.conn = psycopg2.connect(conn_string)
         self.cursor = self.conn.cursor()
-
+    
     def run_query_c_y(self, year):
         self.cursor.execute("select * from census where year = %s;", [year])
         records = self.cursor.fetchall()
